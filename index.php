c682207b9364b8961eec5be26e40074a6a510eec
Ahmed-Hebeshe <ahmedhebeshe@gmail.com>
2025-06-01 13:03:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 if (isset($_GET['url'])) {
     $url = $_GET['url'];
-    $content = @file_get_contents($url);
+
+    $opts = [
+        "http" => [
+            "method" => "GET",
+            "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36\r\n"
+        ]
+    ];
+    $context = stream_context_create($opts);
+    $content = @file_get_contents($url, false, $context);
     echo $content ?: "⚠️ Failed to fetch content.";
 } else {
     echo '<form method="get">
